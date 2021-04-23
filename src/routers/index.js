b3a8d842944c12a9c359ffcddf0a0b8831410684
Mauro Henao <mauro.henaog@gmail.com>
2021-04-23 15:25:48 -0500

    .

--- src/routers/index.js
+++ src/routers/index.js
@@ -32,7 +32,7 @@ export default class Routes extends React.PureComponent {
       <React.Fragment>
         <Router>
           <Switch>
-            <Route exact path="/" component={Theme1} />
+            <Route exact path="/" component={ComingSoon} />
             <Route exact path="/theme1" component={Theme1} />
             <Route exact path="/theme2" component={Theme2} />
             <Route exact path="/theme3" component={Theme3} />
